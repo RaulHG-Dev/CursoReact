5a3bbb5eb0d3a649c1a52a4d17c50aa8a76a46be
Raúl Hidalgo Gaspar <38563016+RaulHG96@users.noreply.github.com>
2023-06-10 12:01:48 -0600

    Semana del 05 al 09 de Junio

--- crm-react/src/components/Layout.jsx
+++ crm-react/src/components/Layout.jsx
@@ -1,13 +1,21 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom"
 
 const Layout = () => {
+  const location = useLocation();
+  
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
         <h2 className="text-4xl font-black text-center text-white">CRM - Cliente</h2>
         <nav className="mt-10">
-          <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to='/'>Cliente</Link>
-          <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to='/clientes/nuevo'>Cliente Nuevo</Link>
+          <Link 
+            className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white' } text-2xl block mt-2 hover:text-blue-300`} 
+            to='/'
+          >Cliente</Link>
+          <Link 
+            className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white' } text-2xl block mt-2 hover:text-blue-300`}
+            to='/clientes/nuevo'
+          >Cliente Nuevo</Link>
         </nav>
       </aside>
 
--- crm-react/src/pages/NuevoCliente.jsx
+++ crm-react/src/pages/NuevoCliente.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { useNavigate, Form } from "react-router-dom"
+import Formulario from "../components/Formulario"
+
+export function action() {
+  console.log('submit');
+  return null;
+}
 
 const NuevoCliente = () => {
+  const navigate = useNavigate()
   return (
-    <div>NuevoCliente</div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo cliente</h1>
+      <p className="mt-3">Llena todos los campos para registrar un nuevo cliente</p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-6 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <Form
+          method="post"
+        >
+          <Formulario/>
+          <input 
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar cliente"
+          />
+        </Form>
+      </div>
+    </>
   )
 }
 
