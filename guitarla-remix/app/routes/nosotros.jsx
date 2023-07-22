6a6c1a5143b1654a6415b5dafcae9308c18e8c61
Raúl Hidalgo Gaspar <38563016+RaulHG96@users.noreply.github.com>
2023-07-22 17:01:57 -0600

    SEMANA DEL 17 AL 21 DE JULIO

--- guitarla-remix/app/routes/nosotros.jsx
+++ guitarla-remix/app/routes/nosotros.jsx
@@ -1,6 +1,37 @@
+import imagen from '../../public/img/nosotros.jpg'
+import styles from '~/styles/nosotros.css'
+
+export function meta() {
+  return [
+      {charset: 'utf-8'},
+      {title: 'GuitarLA - Nosotros'},
+      {viewport: 'width=device-width,initial-scale=1'}
+  ]
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ];
+}
+
 const Nosotros = () => {
   return (
-    <div>Nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="headin">Nosotros</h2>
+
+      <div className="contenido">
+        <img src={imagen} alt="imagen sobre nosotros" />
+
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laboriosam quae nobis voluptatibus aperiam recusandae autem id odio incidunt mollitia modi, repellat numquam perferendis eligendi earum maiores officia officiis ea.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laboriosam quae nobis voluptatibus aperiam recusandae autem id odio incidunt mollitia modi, repellat numquam perferendis eligendi earum maiores officia officiis ea.</p>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- guitarla-remix/app/routes/tienda.jsx
+++ guitarla-remix/app/routes/tienda.jsx
@@ -1,7 +1,46 @@
+import { getGuitarras } from "~/models/guitarras.server";
+import { useLoaderData } from "@remix-run/react";
+import Guitarra from "~/components/guitarra";
+import styles from "~/styles/guitarras.css"
+
+export function meta() {
+  return [
+      {charset: 'utf-8'},
+      {title: 'GuitarLA - Tienda de Guitarras'},
+      {viewport: 'width=device-width,initial-scale=1'}
+  ]
+}
+
+export function links() {
+  return [{
+    rel: 'stylesheet',
+    href: styles
+  }]
+}
+
+export async function loader() {
+  const guitarras = await getGuitarras();
+  return guitarras.data;
+}
 
 const Tienda = () => {
+  const guitarras = useLoaderData();
+  // guitarras.map(i => console.log(i))
   return (
-    <div>Tienda</div>
+    <main className="contenedor">
+      <h2 className="heading">Nuestra Colección</h2>
+
+      {guitarras?.length && (
+        <div className="guitarras-grid">
+          {guitarras.map( guitarra => (
+            <Guitarra
+              key={guitarra?.id}
+              guitarra={guitarra?.attributes}
+            />
+          ))}
+        </div>
+      )}
+    </main>
   )
 }
 
