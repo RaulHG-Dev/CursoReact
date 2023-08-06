6c4db6cf2af3ed9afc4a97c09845a14fe51568d0
Raúl Hidalgo Gaspar <38563016+RaulHG96@users.noreply.github.com>
2023-08-06 15:56:04 -0600

    Semana del 31 de julio al 4 de agosto

--- guitarla-remix/app/routes/blog.jsx
+++ guitarla-remix/app/routes/blog.jsx
@@ -1,7 +1,45 @@
+import { getPosts } from "~/models/posts.server"
+import { useLoaderData } from "react-router";
+import Post from "~/components/post";
+import styles from "~/styles/blog.css"
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ];
+}
+
+export function meta() {
+  return [
+      {charset: 'utf-8'},
+      {title: 'GuitarLA - Nuestro Blog'},
+      {viewport: 'width=device-width,initial-scale=1'}
+  ]
+}
+
+export async function loader() {
+  const posts = await getPosts();
+  return posts.data;
+}
 
 const Blog = () => {
+  const posts = useLoaderData();
+
   return (
-    <div>Blog</div>
+    <main className="contenedor">
+      <h2 className="heading">Blog</h2>
+      <div className="blog"> 
+        {posts.map(post => (
+          <Post
+            key={post.id}
+            post={post.attributes}
+          />
+        ))}
+      </div>
+    </main>
   )
 }
 
---
+++ guitarla-remix/app/routes/posts.$postsUrl.jsx
@@ -0,0 +1,54 @@
+import { formatearFecha } from "~/utils/helpers";
+import { getPost } from "~/models/posts.server";
+import { useLoaderData } from "@remix-run/react";
+import styles from "~/styles/blog.css";
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ];
+}
+
+export function meta({ data }) {
+  if (!data || Object.keys(data).length === 0) {
+    return [{ title: "GuitarLA - Entrada no encontrada" }];
+  }
+
+  return [
+    { charset: "utf-8" },
+    { title: `GuitarLA - ${data.data[0].attributes.titulo}` },
+    { viewport: "width=device-width,initial-scale=1" },
+  ];
+}
+
+export async function loader({params}) {
+    const { postsUrl } = params;
+    const post = await getPost(postsUrl);
+    if(post.data.length === 0) {
+        throw new Response('', {
+            status: 404,
+            statusText: 'Entrada no encontrada'
+        });
+    }
+    return post
+}
+
+function Posts() {
+  const post = useLoaderData();
+	const {titulo, contenido, imagen, publishedAt} = post?.data[0]?.attributes;
+  return (
+    <article className="contenedor post mt-3">
+			<img src={imagen.data.attributes.url} alt={`imagen blog ${titulo}`} className="imagen" />
+			<div className="contenido">
+				<h3>{titulo}</h3>
+				<p className="fecha">{formatearFecha(publishedAt)}</p>
+				<p className="texto">{contenido}</p>
+			</div>
+		</article>
+  )
+}
+
+export default Posts
