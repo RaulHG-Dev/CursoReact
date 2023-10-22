230ce41d6bb12f71a40e9b6778cf05f0e9bcb7ea
Raúl Hidalgo Gaspar <hgraul963@gmail.com>
2023-10-22 13:29:32 -0600

    Video 260. Routing Dinámico en Next.js

---
+++ guitarla-next/pages/guitarras/[url].js
@@ -0,0 +1,10 @@
+import {useRouter} from "next/router"
+
+export default function Producto() {
+    // Acceder a la url dinámica
+    const router = useRouter();
+    // console.log(router)
+    return ( 
+        <div>[url]</div>
+    )
+}
