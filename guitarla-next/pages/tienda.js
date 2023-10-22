47c0e674e82596af0f21b5c07beb737b472e3f80
Raúl Hidalgo Gaspar <hgraul963@gmail.com>
2023-10-22 11:59:13 -0600

    Video 257. Consultar la API con getServerSideProps

--- guitarla-next/pages/tienda.js
+++ guitarla-next/pages/tienda.js
@@ -2,7 +2,7 @@ import Layout from "@/components/layout"
 import ListadoGuitarras from "@/components/listado-guitarras"
 
 export default function Tienda({guitarras}) {
-  console.log(guitarras)
+  // console.log(guitarras)
   return (
     <Layout
       title={'Tienda Virtual'}
@@ -12,14 +12,25 @@ export default function Tienda({guitarras}) {
         <h1 className="heading">Nuestra Colección</h1>
 
         <ListadoGuitarras
-
+          guitarras={guitarras}
         />
       </main>
     </Layout>
   )
 }
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//   const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
+//   const {data: guitarras} = await respuesta.json()
+  
+//   return {
+//     props: {
+//       guitarras
+//     }
+//   }
+// }
+
+export async function getServerSideProps() {
   const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
   const {data: guitarras} = await respuesta.json()
   
