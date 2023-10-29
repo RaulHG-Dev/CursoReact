9de2f90df3bd3b300dd8ebfd319feb411067cf45
Raúl Hidalgo Gaspar <hgraul963@gmail.com>
2023-10-29 10:28:17 -0600

    Video 261. Routing Dinámico con getServerSideProps

--- guitarla-next/pages/guitarras/[url].js
+++ guitarla-next/pages/guitarras/[url].js
@@ -1,10 +1,22 @@
 import {useRouter} from "next/router"
 
-export default function Producto() {
+export default function Producto({guitarra}) {
     // Acceder a la url dinámica
     const router = useRouter();
     // console.log(router)
     return ( 
         <div>[url]</div>
     )
+}
+
+export async function getServerSideProps({query: {url}}) {
+
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`);
+    const {data: guitarra} = await respuesta.json();
+    console.log(data)
+    return {
+        props: {
+            guitarra
+        }
+    }
 }
