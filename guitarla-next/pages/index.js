6729a46ac61d928458d0fb7d577e262c4dfb58d5
Raúl Hidalgo Gaspar <hgraul963@gmail.com>
2023-11-05 11:42:28 -0600

    Video 268. Mostrando Guitarras y Blog en la página de inicio

--- guitarla-next/pages/index.js
+++ guitarla-next/pages/index.js
@@ -1,16 +1,60 @@
 import Layout from "@/components/layout"
-import Link from "next/link"
+import Guitarra from "@/components/guitarra"
+import Post from "@/components/post"
+import styles from "../styles/grid.module.css"
 
-export default function Home() {
+export default function Home({guitarras, posts}) {
   return (
     <>
       <Layout
         title={'Inicio'}
         description={'Blog de música, venta de guitarras y más'}
       >
-        <h1>hola mundo en next</h1>
-        <Link href='/nosotros'>Nosotros</Link>
+        <main className="contenedor">
+          <h1 className="heading">Nuestra Colección</h1>
+          <div className={styles.grid}>
+            {guitarras?.map(guitarra => (
+              // console.log(guitarra);
+              <Guitarra
+                key={guitarra.id}
+                guitarra={guitarra.attributes}
+              />
+            ))}
+          </div>
+        </main>
+        <section className="contenedor">
+          <h2 className="heading">Blog</h2>
+          <div className={styles.grid}>
+            {posts?.map(post => (
+              <Post
+                key={post.id}
+                post={post.attributes}
+              />
+            ))}
+          </div>
+        </section>
       </Layout>
     </>
   )
+}
+
+export async function getStaticProps() {
+    const urlGuitarras = `${process.env.API_URL}/guitarras?populate=imagen`
+    const urlPosts = `${process.env.API_URL}/posts?populate=imagen`
+    const [resGuitarras, resPosts] = await Promise.all([
+      fetch(urlGuitarras),
+      fetch(urlPosts)
+    ])
+
+    const [{data: guitarras}, {data: posts}] = await Promise.all([
+      resGuitarras.json(),
+      resPosts.json()
+    ])
+
+    return {
+      props: {
+        guitarras,
+        posts
+      }
+    }
 }
