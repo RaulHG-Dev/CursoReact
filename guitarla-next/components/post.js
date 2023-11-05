db758eb1c393bf21e9f200f0db297d37cd282258
Raúl Hidalgo Gaspar <hgraul963@gmail.com>
2023-11-05 10:48:52 -0600

    Video 266. CSS al Blog y Función helper de fecha

--- guitarla-next/components/post.js
+++ guitarla-next/components/post.js
@@ -1,5 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
+import { formatearFecha } from "../utils/helpers"
+import styles from "../styles/blog.module.css"
 
 export default function Post({post}) {
     const {contenido, imagen, titulo, url, publishedAt} = post
@@ -7,11 +9,11 @@ export default function Post({post}) {
         <article>
             <Image src={imagen.data.attributes.formats.medium.url} width={600} height={400} alt={`imagen blog ${titulo}`} />
 
-            <div>
+            <div className={styles.contenido}>
                 <h3>{titulo}</h3>
-                <p>{publishedAt}</p>
-                <p>{contenido}</p>
-                <Link href={`/blog/${url}`}>
+                <p className={styles.fecha}>{formatearFecha(publishedAt)}</p>
+                <p className={styles.resumen}>{contenido}</p>
+                <Link href={`/blog/${url}`} className={styles.enlace}>
                     Leer Post
                 </Link>
             </div>
